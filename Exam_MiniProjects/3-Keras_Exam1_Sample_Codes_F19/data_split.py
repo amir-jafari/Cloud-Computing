8a0124aac49ae8c4768a9ee984b60767dabcf193
amir-jafari <amir.h.jafari@okstate.edu>
2019-10-11 08:08:05 -0400

    Updated

--- Exam_MiniProjects/3-Keras_Exam1_Sample_Codes_F19/data_split.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from keras.datasets import mnist
-
-# This script generates the training set (the one you will be provided with),
-# and the held out set (the one we will use to test your model towards the leaderboard).
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-np.save("x_train.npy", x_train); np.save("y_train.npy", y_train)
-np.save("x_test.npy", x_test); np.save("y_test.npy", y_test)
