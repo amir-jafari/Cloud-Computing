3b5c3049f0776e0f151f849cdee259a9444d0ef1
amir-jafari <amir.h.jafari@okstate.edu>
2019-10-22 19:01:10 -0400

    Updated

--- Exam_MiniProjects/3-Keras_Exam1_Sample_Codes_F19/2-train_ajafari.py
+++ Exam_MiniProjects/3-Keras_Exam1_Sample_Codes_F19/2-train_ajafari.py
@@ -46,5 +46,5 @@ model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=N_EPOCHS, validation_d
 
 # %% ------------------------------------------ Final test -------------------------------------------------------------
 print("Final accuracy on validations set:", 100*model.evaluate(x_test, y_test)[1], "%")
-print("Cohen Kappa", cohen_kappa_score(np.argmax(model.predict(x_test),axis=1),np.argmax(y_test,axis=1))
-print("F1 score", f1_score(np.argmax(model.predict(x_test),axis=1),np.argmax(y_test,axis=1), average = 'macro')
+print("Cohen Kappa", cohen_kappa_score(np.argmax(model.predict(x_test),axis=1),np.argmax(y_test,axis=1)))
+print("F1 score", f1_score(np.argmax(model.predict(x_test),axis=1),np.argmax(y_test,axis=1), average = 'macro'))
