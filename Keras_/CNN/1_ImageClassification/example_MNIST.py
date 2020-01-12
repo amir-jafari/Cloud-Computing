55d9d816f1f4e0c00ff53ee419ec1632d2733740
amir-jafari <amir.h.jafari@okstate.edu>
2020-01-12 16:38:00 -0500

    Updated

--- Keras_/CNN/1_ImageClassification/example_MNIST.py
+++ Keras_/CNN/1_ImageClassification/example_MNIST.py
@@ -8,35 +8,54 @@ from keras.layers import Dense, Dropout, BatchNormalization, Conv2D, Flatten, Ma
 from keras.optimizers import Adam
 from keras.datasets import mnist
 from keras.utils import to_categorical
-
-
+from keras import backend as K
+import keras
 # %% --------------------------------------- Set-Up --------------------------------------------------------------------
-SEED = 42
-os.environ['PYTHONHASHSEED'] = str(SEED)
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
+# SEED = 42
+# os.environ['PYTHONHASHSEED'] = str(SEED)
+# random.seed(SEED)
+# np.random.seed(SEED)
+# tf.random.set_seed(SEED)
 
 # %% ----------------------------------- Hyper Parameters --------------------------------------------------------------
 LR = 1e-3
 N_EPOCHS = 30
 BATCH_SIZE = 512
 DROPOUT = 0.5
-
+num_classes = 10
+img_rows, img_cols = 28, 28
 # %% -------------------------------------- Data Prep ------------------------------------------------------------------
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 # Reshapes to (n_examples, n_channels, height_pixels, width_pixels)
-x_train, x_test = x_train.reshape(len(x_train), 1, 28, 28), x_test.reshape(len(x_test), 1, 28, 28)
-y_train, y_test = to_categorical(y_train, num_classes=10), to_categorical(y_test, num_classes=10)
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+# x_train, x_test = x_train.reshape(len(x_train), 1, 28, 28), x_test.reshape(len(x_test), 1, 28, 28)
+# y_train, y_test = to_categorical(y_train, num_classes=10), to_categorical(y_test, num_classes=10)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 # %% -------------------------------------- Training Prep ----------------------------------------------------------
 model = Sequential([
-    Conv2D(16, 3, data_format="channels_first", activation="relu"),
+    Conv2D(16, (3,3),  activation="relu"),
     BatchNormalization(),
-    MaxPooling2D(2),
-    Conv2D(32, 3, data_format="channels_first", activation="relu"),
+    MaxPooling2D((2,2)),
+    Conv2D(32, (3,3), activation="relu"),
     BatchNormalization(),
-    AveragePooling2D(2),
+    AveragePooling2D((2,2)),
     Flatten(),
     Dense(400, activation="tanh"),
     Dropout(DROPOUT),
