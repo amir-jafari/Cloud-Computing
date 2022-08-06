d3904f4f4ada6673079c22a832e5cd997a240822
amir-jafari <amir.h.jafari@okstate.edu>
2022-08-06 15:23:49 -0400

    Updated

--- Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-20.04/V3/install-20-04-part1-V1.sh
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-20.04/V3/install-20-04-part1-V1.sh
@@ -12,21 +12,26 @@
 # work properly.   							                    #
 # ------------------------------------------------------------- #
 
-# ----------------- Install Cuda 11.6 -----------------
+# ----------------- Install Cuda 11.2 -----------------
+sudo apt update
+sudo apt upgrade
+sudo apt install nvidia-driver-390 -y
+sudo apt install nvidia-driver-460 -y
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
 
 sed 1d /etc/environment > /etc/environment
-echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-11.6/bin"' >> /etc/environment
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-11.2/bin"' >> /etc/environment
 
 echo $PATH
-echo 'export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+echo 'export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
 echo 'export DISPLAY=localhost:10.0' >> ~/.bashrc
 
 
