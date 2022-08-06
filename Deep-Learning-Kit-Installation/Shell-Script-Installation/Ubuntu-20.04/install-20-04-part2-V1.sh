e48753a04f361e19a3e2422cdc7cfd17ea151aa2
amir-jafari <amir.h.jafari@okstate.edu>
2022-08-06 17:26:27 -0400

    Updated

--- Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-20.04/install-20-04-part2-V1.sh
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-20.04/install-20-04-part2-V1.sh
@@ -7,16 +7,15 @@
 #Date: 08/06/2022					                      #
 # ------------------------------------------------------------- #
 
-# ----------------- Cudnn 8.2 cuda 11.6-----------------
-wget https://storage.googleapis.com/cuda-deb/libcudnn8_8.1.1.33-1%2Bcuda11.2_amd64.deb
-wget https://storage.googleapis.com/cuda-deb/libcudnn8-dev_8.1.1.33-1%2Bcuda11.2_amd64.deb
-wget https://storage.googleapis.com/cuda-deb/libcudnn8-samples_8.1.1.33-1%2Bcuda11.2_amd64.deb
+# ----------------- Cudnn 8.2 cuda 11.6-------------------------
 
-sudo dpkg -i libcudnn8_8.1.1.33-1+cuda11.2_amd64.deb
-sudo dpkg -i libcudnn8-dev_8.1.1.33-1+cuda11.2_amd64.deb
-sudo dpkg -i libcudnn8-samples_8.1.1.33-1+cuda11.2_amd64.deb
+wget https://storage.googleapis.com/cuda-deb/libcudnn8_8.2.1.32-1%2Bcuda11.3_amd64.deb
+wget https://storage.googleapis.com/cuda-deb/libcudnn8-dev_8.2.1.32-1%2Bcuda11.3_amd64.deb
+wget https://storage.googleapis.com/cuda-deb/libcudnn8-samples_8.2.1.32-1%2Bcuda11.3_amd64.deb
 
-
+sudo dpkg -i libcudnn8_8.2.1.32-1+cuda11.3_amd64.deb
+sudo dpkg -i libcudnn8-dev_8.2.1.32-1+cuda11.3_amd64.deb
+sudo dpkg -i libcudnn8-samples_8.2.1.32-1+cuda11.3_amd64.deb
 
 
 
