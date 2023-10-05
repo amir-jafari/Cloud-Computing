48323c19fe8728d3a71fec3e9d88227996675cab
amir-jafari <amir.h.jafari@okstate.edu>
2023-10-05 16:43:23 -0400

    cuda 8.6 with cudnn

--- Standalone-Linux-Installation/Ubuntu 22.04/3-cudnn.sh
+++ Standalone-Linux-Installation/Ubuntu 22.04/3-cudnn.sh
@@ -1,13 +1,21 @@
 cd installation/
-wget https://storage.googleapis.com/cuda-deb/libcudnn8_8.2.1.32-1%2Bcuda11.3_amd64.deb
-wget https://storage.googleapis.com/cuda-deb/libcudnn8-dev_8.2.1.32-1%2Bcuda11.3_amd64.deb
-wget https://storage.googleapis.com/cuda-deb/libcudnn8-samples_8.2.1.32-1%2Bcuda11.3_amd64.deb
+#wget https://storage.googleapis.com/cuda-deb/libcudnn8_8.2.1.32-1%2Bcuda11.3_amd64.deb
+#wget https://storage.googleapis.com/cuda-deb/libcudnn8-dev_8.2.1.32-1%2Bcuda11.3_amd64.deb
+#wget https://storage.googleapis.com/cuda-deb/libcudnn8-samples_8.2.1.32-1%2Bcuda11.3_amd64.deb
+#
+#sudo dpkg -i libcudnn8_8.2.1.32-1+cuda11.3_amd64.deb
+#sudo dpkg -i libcudnn8-dev_8.2.1.32-1+cuda11.3_amd64.deb
+#sudo dpkg -i libcudnn8-samples_8.2.1.32-1+cuda11.3_amd64.deb
+#source ~/.bashrc
+#source /etc/environment
+#cat /proc/driver/nvidia/version
+#
+#
+wget https://storage.googleapis.com/cuda-deb/cudnn-local-repo-ubuntu2204-8.6.0.163_1.0-1_amd64.deb
 
-sudo dpkg -i libcudnn8_8.2.1.32-1+cuda11.3_amd64.deb
-sudo dpkg -i libcudnn8-dev_8.2.1.32-1+cuda11.3_amd64.deb
-sudo dpkg -i libcudnn8-samples_8.2.1.32-1+cuda11.3_amd64.deb
-source ~/.bashrc
-source /etc/environment
-cat /proc/driver/nvidia/version
-
-
+sudo dpkg -i cudnn-local-repo-ubuntu2204-8.6.0.163_1.0-1_amd64.deb
+sudo cp /var/cudnn-local-repo-ubuntu2204-8.6.0.163/cudnn-local-FAED14DD-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get install libcudnn8=8.6.0.163-1+cuda11.8
+sudo apt-get install libcudnn8-dev=8.6.0.163-1+cuda11.8
+sudo apt-get install libcudnn8-samples=8.6.0.163-1+cuda11.8
