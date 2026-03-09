1ed97fef0237f0fd0493c22e97405965ac2d6eef
amir-jafari <amir.h.jafari@okstate.edu>
2026-03-09 19:33:15 -0400

    Update installation scripts for CUDA 12.6.2, CUDNN 9.6.0, and NVIDIA Driver 565.77 on Ubuntu 24.04. Expand Python 3.12 setup and refine package installations for TensorFlow, PyTorch, and additional scientific libraries.

--- Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-24.04/Temp/install-24-04-part0-V3.sh
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-24.04/Temp/install-24-04-part0-V3.sh
@@ -5,7 +5,7 @@
 #---------------------------------------------------------------#
 #Autor: Amir Jafari		                                          #
 #Date: 07/04/2025				                                        #
-# Nvidia Driver Version: 565.77                                 #
+# Nvidia Driver Version: 560.35.03                              #
 #---------------------------------------------------------------#
 # ----------------- Install Nvidia Driver------------------------
 sudo apt purge nvidia* -y
@@ -16,7 +16,7 @@ sudo rm -rf /usr/local/cuda*
 sudo apt update && sudo apt upgrade -y
 sudo apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
 sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt install nvidia-driver-565
+sudo apt install nvidia-driver-560
 sudo reboot
 
 
---
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-24.04/Temp/install-24-04-part1-V3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# --------------------------------------------------------------#
+# Script to set up a Deep Learning VM on Google Cloud Platform  #
+#---------------------------------------------------------------#
+# Autor: Amir Jafari		                                        #
+# Date: 07/04/2025				                                      #
+# Cuda Version: 12.5                                            #
+#---------------------------------------------------------------#
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.5.0/local_installers/cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-5-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-5
+
+
+
+
+
+
